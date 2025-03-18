d44133ca974eed6b61ba06afa54b39e4a4a76755
Roby Tanama <robytanma@gmail.com>
2025-03-18 18:22:54 +0700

    fix: fix next config remove capacitor

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  distDir: "build",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
