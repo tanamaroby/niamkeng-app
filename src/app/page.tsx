39106ee5221dacb8967b849d86ce8bbdc7f00174
Roby Tanama <robytanma@gmail.com>
2025-04-18 13:02:04 +0700

    feat: add rinpochest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import NavCard from "@/components/nav-card";
 import StaticCard from "@/components/static-card";
 import { DEDICATION_PRAYER, MULTIPLY_MANTRA } from "@/lib/constants";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="p-4 h-full flex flex-grow items-center justify-center flex-col gap-8">
+      <p className="text-2xl font-bold">FBC Chant Application</p>
+      <div className="flex flex-col gap-2 items-center justify-center">
+        <Image
+          src="/Rinpochest.jpeg"
+          alt="Passang Rinpoche Profile"
+          height={500}
+          width={300}
+          className="rounded-lg shadow-md ring-2 ring-accent-foreground"
+        />
+        <p className="italic">Passang Rinpoche</p>
+      </div>
       <StaticCard {...MULTIPLY_MANTRA} />
       <div className="flex flex-col gap-2 text-center">
         <p className="text-lg font-semibold text-primary">
