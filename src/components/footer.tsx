38bfc0a2d11e6c8d0f22ff288f42c135816fd2ac
Roby Tanama <robytanma@gmail.com>
2025-04-18 14:29:16 +0700

    fix: Update footer arrangement

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,13 @@
 export default function Footer() {
   return (
     <footer className="p-6 w-full bg-sidebar flex flex-col items-center text-center text-sm space-y-2">
-      <p className="font-semibold tracking-wide">
+      <div className="flex items-center gap-1 text-sm">
+        <span>Dedicated to</span>
+        <span className="font-extrabold text-base">Passang Rinpoche</span>
+      </div>
+      <p className="font-semibold tracking-wide text-muted-foreground">
         Made by <span className="font-bold">Roby Tanama</span>
       </p>
-
-      <div className="flex items-center gap-1 text-sm text-muted-foreground">
-        <span className="italic">Dedicated to</span>
-        <span className="font-medium">Passang Rinpoche</span>
-      </div>
 
       <div className="flex items-center gap-1">
         <span className="text-sm">From</span>
